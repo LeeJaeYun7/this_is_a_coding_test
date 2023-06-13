926696e217b5dd57e6e38c20ced834b81164658f
zzzso-o <1211ditto@gmail.com>
2023-06-13 17:39:59 +0900

    [3-2]큰 수의 법칙_15min

--- 1주차/이지수/큰 수의 법칙.py
+++ 1주차/이지수/큰 수의 법칙.py
@@ -3,7 +3,7 @@ sys.stdin = open("input1.txt")
 n, m, k = map(int, input().split())
 numbers = list(map(int, input().split()))
 
-numbers.sort() # 큰 순으로 정렬
+numbers.sort() # 오름차순으로 정렬
 # print(numbers)
 result = 0
 while True:
@@ -12,9 +12,10 @@ while True:
             break
         result += numbers[-1]
         m -= 1
-    if m == 0:
+    if m == 0: # for문 밖에도 break걸어주는 것이 포인트
         break
     result += numbers[-2]
-    m -= 1
+    m -=1
+
 
 print(result)

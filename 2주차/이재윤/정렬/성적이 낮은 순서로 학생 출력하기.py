c090b4d13c7936957be8b079e5da21ff5783ca49
LeeJaeYun <134198681+LeeJaeYun7@users.noreply.github.com>
2023-06-21 21:25:17 +0900

    Create 성적이 낮은 순서로 학생 출력하기.py

---
+++ 2주차/이재윤/정렬/성적이 낮은 순서로 학생 출력하기.py
@@ -0,0 +1,17 @@
+N = int(input())
+
+array = []
+
+for i in range(N):
+    // input.split()으로 입력 받는다
+    input_data = input().split()
+    name = input_data[0]
+    point = int(input_data[1])
+    array.append((name, point))
+
+// 람다 함수를 활용해서 student[1]로 정렬해준다. 
+array = sorted(array, key=lambda student:student[1])
+
+// array를 순회하면서, data[0]을 출력한다. 
+for data in array:
+    print(data[0], end=' ')

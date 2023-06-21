cfd4eb3ae713b28ef119ba2799f40fb957c55528
LeeJaeYun <134198681+LeeJaeYun7@users.noreply.github.com>
2023-06-21 21:25:48 +0900

    Update 위에서 아래로.py

--- 2주차/이재윤/정렬/위에서 아래로.py
+++ 2주차/이재윤/정렬/위에서 아래로.py
@@ -4,7 +4,8 @@ array = []
 for i in range(N):
     num = int(input())
     array.append(num)
-    
+
+// sorted와 reverse=True를 이용해서 내림차순 정렬을 한다 
 array = sorted(array, reverse=True)
 
 for i in array:

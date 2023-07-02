1ae5bda70bb7f05534812d2d4b94e6f17e5b4be9
LeeJaeYun <134198681+LeeJaeYun7@users.noreply.github.com>
2023-07-02 19:41:04 +0900

    Update 이재윤.py

--- 4주차/모험가 길드/이재윤.py
+++ 4주차/모험가 길드/이재윤.py
@@ -1,3 +1,24 @@
+N = int(input())
 
+arr = list(map(int, input().split()))
 
-```
+arr.sort()
+ans = 0 
+
+    
+pos = 0
+tmp = []
+
+while pos<N:
+    
+    num = arr[pos]
+    tmp.append(num)
+    
+    if max(tmp) == len(tmp):
+        ans += 1
+        tmp = [] 
+    
+    pos += 1
+    
+    
+print(ans)

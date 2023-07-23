03293907006a6be19df20622a9d5f86d04465962
JeongHanLim <jhlim271@naver.com>
2023-07-23 19:01:41 +0900

    still fail

--- 5주차/경쟁적 전염/임정한_실패.py
+++ 5주차/경쟁적 전염/임정한_실패.py
@@ -1,3 +1,4 @@
+from collections import deque
 N, K = map(int, input().split())
 is_valid = lambda x, y: 0 <= x < N and 0 <= y < N if True else False
 
@@ -13,8 +14,18 @@ for i in range(N):
             virus_dict[virus_map[i][j]].append((i, j))
 
 for second in range(S):
+    q = deque([])
     for virus_type in virus_dict.keys():
-        virus_dict[virus_type]
+        for element in virus_dict[virus_type]:
+            q.append((element, virus_type))
+    while q:
+        position, virus_type = q.pop()
+        x, y = position
+        dx, dy = [1, 0, -1, 0], [0, 1, 0, -1]
+        for i in range(4):
+            if is_valid(x+dx[i], y+dy[i]) and virus_map[x+dx[i]][y+dy[i]] == 0:
+                virus_map[x+dx[i]][y+dy[i]] = virus_type
+
+print(virus_dict[X-1][Y-1])
 
 
-bfs(starting)
